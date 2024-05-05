ee96c49766205d341a79484969f24a846140be2d
Vasyl Mandik <Vasik_ua@ukr.net>
2024-05-04 21:52:02 -0500

    redux

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -11,10 +11,7 @@ const contactsSlice = createSlice({
                 { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
                 { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
                 { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-                { id: 'id-11', name: 'Rosie Simpson', number: '459-12-56' },
-                { id: 'id-21', name: 'Hermione Kline', number: '443-89-12' },
-                { id: 'id-31', name: 'Eden Clements', number: '645-17-79' },
-                { id: 'id-41', name: 'Annie Copeland', number: '227-91-26' },]
+                ]
         },
        
     },
